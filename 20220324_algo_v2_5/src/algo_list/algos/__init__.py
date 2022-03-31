00c5851a728181332d9939a4e54248bca64462f3
liuhaozhe6788 <48321861+liuhaozhe6788@users.noreply.github.com>
2022-03-31 18:21:51 +0800

    Add files via upload

---
+++ 20220324_algo_v2_5/src/algo_list/algos/__init__.py
@@ -0,0 +1,6 @@
+# encoding:utf-8
+"""
+    该package用于实现所有推荐算法，包括广义协同过滤和基于物品的协同过滤
+"""
+from .generalized_cf import GeneralizedCF
+from .item_cf import ItemCF
---
+++ 20220324_algo_v2_5/src/main.py
@@ -0,0 +1,22 @@
+# encoding:utf-8
+import os
+import pandas as pd
+import sys
+sys.dont_write_bytecode = True
+
+
+import configs
+import database
+import visualization
+import utils
+from algo_list import run_all_users
+
+
+if __name__ == "__main__":
+    visualization.cleaned_data_vis("data_20220222.xlsx")
+
+    mydb = database.DataBase(os.path.join(configs.data_folder_path, "data_20220222.xlsx"))
+
+
+    # visualization.club_dist_vis(gama_df, mydb)
+    # visualization.accu_vis(gama_df, mydb)
