d79594a658e1a42127e8d2c48fb2f3b647351f37
liuhaozhe6788 <48321861+liuhaozhe6788@users.noreply.github.com>
2022-04-12 07:57:33 +0800

    Add files via upload

---
+++ 20220410_algo_v2_7/src/main.py
@@ -0,0 +1,20 @@
+# encoding:utf-8
+import os
+import pandas as pd
+import sys
+sys.dont_write_bytecode = True
+
+import configs
+import database
+import visualization
+from algos import AlgosOperator
+import utils
+
+
+if __name__ == "__main__":
+    # visualization.cleaned_data_vis("data_20220222.xlsx")
+
+    mydb = database.DataBase(os.path.join(configs.data_folder_path, "data_20220222.xlsx"))
+
+    algos = AlgosOperator(mydb)
+    algos.calc_and_store_metrics(50)
